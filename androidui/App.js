f902ea4d3821b5407730b82506d484b7ec0e90a2
Aman Soni <66624836+aman5oni@users.noreply.github.com>
2022-08-17 14:00:32 +0530

    "Onboard"

---
+++ androidui/App.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+import React from "react";
+import OnBoard from "./src/screens/OnBoard";
+
+const App = () => {
+  return <OnBoard />;
+};
+
+export default App;
